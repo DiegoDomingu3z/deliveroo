55a42922b5185fa851ae2bb6edf9c994e94ce6bc
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-10-22 01:53:36 -0600

    so much work ahhhh, I'm tired

--- components/Categories.js
+++ components/Categories.js
@@ -1,20 +1,31 @@
+// @ts-nocheck
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CategoryCard from './CategoryCard'
+import sanityClient, { urlFor } from '../sanity'
 
 const Categories = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        sanityClient.fetch(`
+    *[_type == "category"]
+    `).then((data) => {
+            setCategories(data)
+        })
+    }, [])
+    console.log(categories)
     return (
         <ScrollView contentContainerStyle={{
             paddingHorizontal: 15,
             paddingTop: 10,
         }}
             horizontal showsHorizontalScrollIndicator={false} >
-            <CategoryCard imgUrl='https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' title="testing" />
-            <CategoryCard imgUrl='https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' title="testing" />
-            <CategoryCard imgUrl='https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' title="testing" />
-            <CategoryCard imgUrl='https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' title="testing" />
-            <CategoryCard imgUrl='https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' title="testing" />
-            <CategoryCard imgUrl='https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' title="testing" />
+            {categories.map((category) => (
+
+                <CategoryCard key={category._id} imgUrl={urlFor(category.image).url()} title={category.name} />
+            ))}
+
         </ScrollView>
     )
 }
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Icon } from "@rneui/themed";
 import { urlFor } from '../sanity';
+import { useNavigation } from '@react-navigation/native';
 
 const RestaurantCard = ({
     id,
@@ -16,8 +17,25 @@ const RestaurantCard = ({
     long,
     lat,
 }) => {
+
+    const navigation = useNavigation()
     return (
-        <TouchableOpacity className="bg-white mr-3 shadow">
+        <TouchableOpacity
+            onPress={() => {
+                navigation.navigate('Restaurant', {
+                    id,
+                    imgUrl,
+                    title,
+                    rating,
+                    genre,
+                    address,
+                    short_description,
+                    dishes,
+                    long,
+                    lat,
+                })
+            }}
+            className="bg-white mr-3 shadow">
             <Image source={{ uri: urlFor(imgUrl).url() }}
                 className="h-36 w-66 rounded-sm" />
             <View>
